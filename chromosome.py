80d775c75a8e9a16873e97e3347e9532946c0885
jackthias <jackthias@icloud.com>
2017-09-30 16:27:03 -0700

    chromosome.py: Created file for managing chromosomes with two functions: * make_chromosome: takes arguments and places them into a properly formatted list. * fits_chromosome: checks if an entry matches a chromosome.

---
+++ chromosome.py
@@ -0,0 +1,39 @@
+import numbers
+
+
+def make_chromosome(day_1_low, day_1_high, day_2_low, day_2_high, buy_or_short):
+    assert isinstance(day_1_low, numbers.Number), "day_1_low, %r, is not a number." % day_1_low
+    assert isinstance(day_2_low, numbers.Number), "day_2_low, %r, is not a number." % day_2_low
+    assert isinstance(day_1_high, numbers.Number), "day_1_high, %r, is not a number." % day_1_high
+    assert isinstance(day_2_high, numbers.Number), "day_2_high, %r, is not a number." % day_2_high
+    assert (isinstance(buy_or_short, int) and (buy_or_short == 0 or buy_or_short == 1)) \
+        or isinstance(buy_or_short, bool), "buy_or_short is not bool or int"
+
+    if day_1_low > day_1_high:
+        day_1_low, day_1_high = day_1_high, day_2_high
+
+    if day_2_low > day_2_high:
+        day_2_low, day_2_high = day_2_high, day_2_low
+
+    assert day_1_low <= day_1_high, "day_1_low is not the smaller number. (%r, %r)" % (day_1_low, day_1_high)
+    assert day_2_low <= day_2_low, "day_2_low is not the smaller number. (%r, %r)" % (day_2_low, day_2_high)
+    return [day_1_low, day_1_high, day_2_low, day_2_high, bool(buy_or_short)]
+
+
+def fits_chromosome(file, chromosome):
+    assert isinstance(file, list) and len(file) == 3, "file isn't in the right format: %r" % file
+    assert isinstance(chromosome, list) \
+        and len(chromosome) == 5, "chromosome isn't in the right format: %r" % chromosome
+
+    i = 0
+    for day in file[:-1]:
+        if not chromosome[i] <= day <= chromosome[i + 1]:
+            return False
+        i += 2
+    return True
+
+test = False
+if test:
+    file_test = [-0.12, -0.81, -6.80]
+    chromosome_test = make_chromosome(-5, 0, -0.9, 3.1, 1)
+    print(fits_chromosome(file_test, chromosome_test))
