d761182005b56d33488977a9a0dab67b0ce98974
jackthias <jackthias@icloud.com>
2017-09-30 19:24:30 -0700

    create_population in population.py: Creates a population of n, random chromosomes.

--- population.py
+++ population.py
@@ -15,3 +15,9 @@ def get_buy_short():
 
 def random_chromosome():
     return make_chromosome(get_high_low(), get_high_low(), get_high_low(), get_high_low(), get_buy_short())
+
+
+def create_population(n):
+    population = list()
+    for _ in range(0, n):
+        population.append(random_chromosome())
