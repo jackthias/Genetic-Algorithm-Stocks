b596eebae7d9ace67c38f14b7213f07b9e94a105
jackthias <jackthias@icloud.com>
2017-09-30 18:04:31 -0700

    population.py: Created file for generating the population with three functions: * get_high_low: Returns a random number using standard deviation * get_buy_short: Returns true or false randomly * random_chromosome: Returns a chromosome with randomly assigned values.

---
+++ population.py
@@ -0,0 +1,17 @@
+import random
+from chromosome import make_chromosome
+
+standard_var = 1.15
+mean = 0
+
+
+def get_high_low():
+    return random.normalvariate(mean, standard_var)
+
+
+def get_buy_short():
+    return random.choice([True, False])
+
+
+def random_chromosome():
+    return make_chromosome(get_high_low(), get_high_low(), get_high_low(), get_high_low(), get_buy_short())
